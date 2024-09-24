4d29b45428222baf6e1e294c6b6c6fb125777009
RivaldiYN <88421969+RivaldiYN@users.noreply.github.com>
2024-09-24 21:53:14 +0700

    fix: max file #2

---
+++ database/seeders/DokumenSDGsSeeder.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Pencapaian;
+use App\Models\Kecamatan;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DokumenSDGsSeeder {
+    
+}
